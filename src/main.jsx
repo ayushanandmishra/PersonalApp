ed2b9026e5492ac6c5a243b0ad1a1a898df1aca9
ayushanandmishra <ayushanandmishra@gmail.com>
2024-01-30 23:59:27 +0530

    added link

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <BrowserRouter basename="PersonalApp">
+    <BrowserRouter basename="testing">
     <App />
     </BrowserRouter>
    
